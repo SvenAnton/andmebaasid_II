f455fbb48610e7165d4deb9441c0c847fae436a0
Viktor Pavlov <vvpavlov19@gmail.com>
2020-12-18 00:10:53 +0200

    changeset 12 16

--- views/v_koondaruanne.sql
+++ views/v_koondaruanne.sql
@@ -1,11 +1,10 @@
-CREATE OR REPLACE VIEW v_koondaruanne AS
-
+CREATE OR REPLACE VIEW v_auto_koondaruanne AS
 SELECT asl.auto_seisundi_liigi_kood,
-       upper(asl.nimetus)                as status,
-       count(a.auto_seisundi_liigi_kood) as arv
-from auto a
-         right join auto_seisundi_liik asl on a.auto_seisundi_liigi_kood = asl.auto_seisundi_liigi_kood
-group by asl.auto_seisundi_liigi_kood, upper(asl.nimetus)
-order by count(a.auto_seisundi_liigi_kood) desc, upper(asl.nimetus);
+       upper(asl.nimetus::text)          AS auto_seisundi_liigi_nimetus,
+       count(a.auto_seisundi_liigi_kood) AS arv
+FROM auto a
+         RIGHT JOIN auto_seisundi_liik asl ON a.auto_seisundi_liigi_kood = asl.auto_seisundi_liigi_kood
+GROUP BY asl.auto_seisundi_liigi_kood, (upper(asl.nimetus::text))
+ORDER BY (count(a.auto_seisundi_liigi_kood)) DESC, (upper(asl.nimetus::text));
 
-COMMENT ON VIEW v_koondaruanne IS 'Juhata näeb iga Auto seisundi kohta selle koodi, nimetust ja selles seisundis olevate Autode arvu. Kui seisundiga pole seotud ühtegi Autod, siis on see arv 0.';
+COMMENT ON VIEW V_AUTO_KOONDARUANNE IS 'Juhata näeb iga Auto seisundi kohta selle koodi, nimetust ja selles seisundis olevate Autode arvu. Kui seisundiga pole seotud ühtegi Autod, siis on see arv 0.';
